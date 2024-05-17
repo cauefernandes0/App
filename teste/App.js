30562a13198e249ac4c43fd16bf2cec06e6e343d
Felipe Kenji <felipekanachiro@gmail.com>
2024-05-17 16:42:45 -0300

    papa

--- teste/App.js
+++ teste/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, FlatList, Animated,ScrollView, Image } from 'react-native';
 import { useRef } from 'react';
 
+
 const H_MAX_HEIGHT = 200;
 const H_MIN_HEIGHT = 55;
 const H_SCROLL_DISTANCE = H_MAX_HEIGHT - H_MIN_HEIGHT;
